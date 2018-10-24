d9e26715b5fc2fef763fb972e18dbd2cdcaaae88
ChisimChisim <36910475+ChisimChisim@users.noreply.github.com>
2018-10-24 16:04:18 -0500

    completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -13,16 +14,24 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(1000))
+    reg_date = db.Column(db.DateTime)
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, reg_date=None):
         self.title = title
         self.body = body
-
+        if reg_date is None:
+            reg_date = datetime.utcnow()
+        self.reg_date = reg_date
 
 @app.route('/blog')
 def index():
-    blogs = Blog.query.all()
-    return render_template('blog_list.html',title="Buid a Blog", blogs=blogs)
+    if request.args:
+        id = request.args.get('id')
+        blog = Blog.query.filter_by(id=id).first()
+        return render_template('blog_entry.html',title="blog.title", blog=blog) 
+    else:
+        blogs = Blog.query.order_by(db.desc(Blog.reg_date)).all()
+        return render_template('blog_list.html',title="Buid a Blog", blogs=blogs)
 
 @app.route('/blog', methods=['POST'])
 def add_blog():
@@ -42,9 +51,17 @@ def add_blog():
         new_blog = Blog(blog_title, blog_body)
         db.session.add(new_blog)
         db.session.commit()
+        db.session.flush()
+        reg_id = new_blog.id
 
-    blogs = Blog.query.all()
-    return render_template('blog_list.html',title="Buid a Blog", blogs=blogs) 
+    return redirect('/blog?id=' + str(reg_id))
+
+@app.route('/blog', methods=['GET'])
+def show_blog():
+    id = request.args.get('id')
+    blog = Blog.query.filter_by(id=id).first()
+    return render_template('blog_entry.html',title="blog.title", blog=blog) 
+
 
 @app.route('/newpost')
 def newpost_form():
